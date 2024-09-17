7f73ceece1b24ad2b7203dbd751cb5958f5b7fee
pnski <s1l3nce@hotmail.de>
2024-09-17 18:23:31 +0200

    _u

--- scripts/worker/wX2J.js
+++ scripts/worker/wX2J.js
@@ -4,18 +4,20 @@ importScripts("/scripts/lib/jspath/jsonpath.min.js"); //v1.1.1
 
 importScripts("/scripts/lib/dataBaseMod.js");
 
+const pOptions = {
+	ignoreAttributes: true,
+	//allowBooleanAttributes: true,
+	//parseNodeValue: false,
+	//parseAttributeValue: false, //no attributes in anno
+	trimValues: true,
+	processEntities: false, //default :true
+	ignoreDeclaration: true
+};
+
+const parser = new fxp.XMLParser(pOptions);
+
 onmessage = async function (event) {
-	const pOptions = {
-		ignoreAttributes: true,
-		//allowBooleanAttributes: true,
-		//parseNodeValue: false,
-		//parseAttributeValue: false, //no attributes in anno
-		trimValues: true,
-		processEntities: false, //default :true
-		ignoreDeclaration: true
-	};
 	const { xmlData, parentTag } = event.data;
-	const parser = new fxp.XMLParser(pOptions);
 	let parsedXML = await parser.parse(xmlData);
 	const result = jsonpath.query(parsedXML, "$.." + parentTag);
 	await storeInIndexedDB(parentTag, result);
